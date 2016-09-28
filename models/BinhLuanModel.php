41c2d01eed6f1bb772860f486f24c523306a12f4
lequan <ldminhquan0206@gmail.com>
2016-09-28 10:52:10 +0700

    update BinhLuanModel

--- models/BinhLuanModel.php
+++ models/BinhLuanModel.php
@@ -4,13 +4,13 @@ class BinhLuanModel extends Database
 {
     public function ThemBinhLuan($tieu_de, $noi_dung, $khach_hang_id, $sach_id)
     {
-        $chuoiSQL='INSERT INTO binh_luan(`tieu_de`, `noi_dung`, `khach_hang_id`, `sach_id`, `date`) VALUES (?,?,?,?,now())';
+        $chuoiSQL = 'INSERT INTO binh_luan(`tieu_de`, `noi_dung`, `khach_hang_id`, `sach_id`, `date`) VALUES (?,?,?,?,now())';
         $this->setQuery($chuoiSQL);
         return $this->execute(array($tieu_de, $noi_dung, $khach_hang_id, $sach_id));
     }
     public function DSBinhLuan($sach_id)
     {
-        $chuoiSQL='SELECT `binh_luan`.`tieu_de`,`binh_luan`.`noi_dung`,`binh_luan`.`date`, `khach_hang`.`ten_khach_hang`, `khach_hang`.`avatar`
+        $chuoiSQL = 'SELECT `binh_luan`.`tieu_de`,`binh_luan`.`noi_dung`,`binh_luan`.`date`, `khach_hang`.`ten_khach_hang`, `khach_hang`.`avatar`
           FROM `binh_luan` 
           INNER JOIN  `khach_hang` ON `binh_luan`.`khach_hang_id`=`khach_hang`.`ma_khach_hang` 
           Where `binh_luan`.`sach_id` =?';
