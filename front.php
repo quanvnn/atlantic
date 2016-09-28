5f12d5a3d406d404189e925a053945368f2cef9b
lequan <ldminhquan0206@gmail.com>
2016-09-28 11:00:43 +0700

    update files admin.php and front.php

--- front.php
+++ front.php
@@ -1,27 +1,39 @@
 <?php
 include_once('controllers/FrontController.php');
 $FrontController = new FrontController();
-if(isset($_GET['kihieu']))
-{
+if (isset($_GET['kihieu'])) {
 	//echo $_GET['kihieu']; exit();
     //echo $_GET['page']; exit();
-    switch ($_GET['kihieu'])
-    {
-        case 'ho-tro-khach-hang':$FrontController->HoTroKhachHang(); break;
-        case 'gui-yeu-cau':$FrontController->GuiYeuCau(); break;
-        case 'dang-ky':$FrontController->DangKy(); break;
-        case 'gio-hang':$FrontController->ThongTinGioHang(); break;
-        case 'huy-gio-hang':$FrontController->HuyGioHang(); break;
-        case 'dat-hang':$FrontController->DatHang(); break;
-        case 'dang-xuat':$FrontController->DangXuat(); break;
-        case 'thong-tin-don-hang':$FrontController->ThongTinDonHang(); break;
-        case 'quen-mat-khau':$FrontController->QuenMatKhau(); break;
+    switch ($_GET['kihieu']) {
+        case 'ho-tro-khach-hang':
+            $FrontController->HoTroKhachHang(); break;
+        case 'gui-yeu-cau':
+            $FrontController->GuiYeuCau(); break;
+        case 'dang-ky':
+            $FrontController->DangKy(); break;
+        case 'gio-hang':
+            $FrontController->ThongTinGioHang(); break;
+        case 'huy-gio-hang':
+            $FrontController->HuyGioHang(); break;
+        case 'dat-hang':
+            $FrontController->DatHang(); break;
+        case 'dang-xuat':
+            $FrontController->DangXuat(); break;
+        case 'thong-tin-don-hang':
+            $FrontController->ThongTinDonHang(); break;
+        case 'quen-mat-khau':
+            $FrontController->QuenMatKhau(); break;
         
-        case 'san-pham-loai-cha':$FrontController->SanPhamTheoLoaiCha(); break;
-        case 'san-pham-loai-con':$FrontController->SanPhamTheoLoaiCon(); break;
-        case 'chi-tiet-san-pham':$FrontController->ChiTietSanPham(); break;
-        case 'san-pham-theo-chu-de':$FrontController->SanPhamTheoChuDe(); break;
-        case 'reset-password':$FrontController->ResetPassWord(); break;
+        case 'san-pham-loai-cha':
+            $FrontController->SanPhamTheoLoaiCha(); break;
+        case 'san-pham-loai-con':
+            $FrontController->SanPhamTheoLoaiCon(); break;
+        case 'chi-tiet-san-pham':
+            $FrontController->ChiTietSanPham(); break;
+        case 'san-pham-theo-chu-de':
+            $FrontController->SanPhamTheoChuDe(); break;
+        case 'reset-password':
+            $FrontController->ResetPassWord(); break;
     }
 }
 else
