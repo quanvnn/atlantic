f58280f89b5c6bd713c1d3cd131166b9b826b30d
lequan <ldminhquan0206@gmail.com>
2016-09-28 10:41:39 +0700

    update file smartycontroller

--- controllers/SmartyController.php
+++ controllers/SmartyController.php
@@ -1,9 +1,10 @@
 <?php
 session_start();
 chdir(dirname(__DIR__));
-define('path','http://atlantic.dev');
-require_once('smarty/libs/Smarty.class.php');
-require_once('models/LoaiSanPhamModel.php');
+define('path', 'http://atlantic.dev');
+require_once "smarty/libs/Smarty.class.php";
+require_once "models/LoaiSanPhamModel.php";
+//require_once('models/LoaiSanPhamModel.php');
 include_once('library/Gio_hang.php');
 class SmartyController extends Smarty
 {
@@ -14,16 +15,15 @@ class SmartyController extends Smarty
         $this->setCompileDir('views/templates_c');
         $this->setCacheDir('views/caches');
         $this->setConfigDir('views/configs');
-        $this->assign('path',path);
+        $this->assign('path', path);
 
         $LoaiSanPhamModel = new LoaiSanPhamModel();
         $DSLoaiSanPham = $LoaiSanPhamModel->DSLoaiSanPham(); 
         //var_dump($DSLoaiSanPham); exit();
-        $this->assign('DSLoaiSanPham',$DSLoaiSanPham);
+        $this->assign('DSLoaiSanPham', $DSLoaiSanPham);
 
         //check submit form đăng nhập header
-        if(isset($_POST['btnDangNhap']))
-        {
+        if (isset($_POST['btnDangNhap'])) {
             $email = addslashes($_POST['email']);
             $mat_khau = addslashes($_POST['mat_khau']);
 
@@ -31,13 +31,13 @@ class SmartyController extends Smarty
             $dataKhachHang = $KhachHangModel->getKhachHangDangNhap($email, $mat_khau);
 
             //var_dump($dataKhachHang); exit();
-            if($dataKhachHang)
-            {
+            if ($dataKhachHang) {
                 // tạo session.khachhang
                 $arrTTKhachHang = array(
-                                'ma_khach_hang'   =>$dataKhachHang['ma_khach_hang'],
-                                'ten_khach_hang'  =>$dataKhachHang['ten_khach_hang'],
-                                'email'           =>$email);
+                                'ma_khach_hang'  => $dataKhachHang['ma_khach_hang'],
+                                'ten_khach_hang' => $dataKhachHang['ten_khach_hang'],
+                                'email'          => $email
+                                );
                 $_SESSION['khachhang'] = $arrTTKhachHang;
                 //var_dump($_SESSION['khachhang']); exit();
             }
