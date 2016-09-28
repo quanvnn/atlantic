287a9401847a63a04e64d3b7f7c77fc0f7a866d6
lequan <ldminhquan0206@gmail.com>
2016-09-28 10:51:12 +0700

    update file Adminmoles

--- models/AdminModel.php
+++ models/AdminModel.php
@@ -2,10 +2,10 @@
 include_once('models/Database.php');
 class AdminModel extends Database
 {
-    public function getNguoiDungDangNhap($tendangnhap,$matkhau)
+    public function getNguoiDungDangNhap($tendangnhap, $matkhau)
     {
         $this->setQuery('SELECT * FROM nguoi_dung WHERE ten_dang_nhap=? AND mat_khau=?');
-        return $this->loadRow(array($tendangnhap,$matkhau));
+        return $this->loadRow(array($tendangnhap, $matkhau));
     }
     // Bang san pham
     public function DSSanPhamAdmin()
@@ -20,13 +20,13 @@ class AdminModel extends Database
     }
     public function ThemSanPham($data)
     {
-        $chuoiSQL='INSERT INTO san_pham(`ten_san_pham`, `ten_san_pham_url`, `ma_loai`, `mo_ta`, `gia_bia`, `gia_ban`, `hinh`, `ngay_tao`, `chu_de_id`) VALUES (?,?,?,?,?,?,?,now(),?)';
+        $chuoiSQL = 'INSERT INTO san_pham(`ten_san_pham`, `ten_san_pham_url`, `ma_loai`, `mo_ta`, `gia_bia`, `gia_ban`, `hinh`, `ngay_tao`, `chu_de_id`) VALUES (?,?,?,?,?,?,?,now(),?)';
         $this->setQuery($chuoiSQL);
         return $this->execute(array($data['ten_san_pham'], $data['ten_san_pham_url'], $data['ma_loai'], $data['mo_ta'], $data['gia_bia'], $data['gia_ban'], $data['hinh'], $data['chu_de_id']));
     }
     public function CapNhatSanPham($data)
     {
-        $chuoiSQL="UPDATE san_pham SET ten_san_pham=?, ten_san_pham_url=?, ma_loai=?, mo_ta=?, gia_bia=?, hinh=?, gia_ban=?, ngay_tao=now(), chu_de_id=? WHERE ma_san_pham=?";
+        $chuoiSQL = "UPDATE san_pham SET ten_san_pham=?, ten_san_pham_url=?, ma_loai=?, mo_ta=?, gia_bia=?, hinh=?, gia_ban=?, ngay_tao=now(), chu_de_id=? WHERE ma_san_pham=?";
         $this->setQuery($chuoiSQL);
         return $this->execute(array($data['ten_san_pham'], $data['ten_san_pham_url'], $data['ma_loai'], $data['mo_ta'], $data['gia_bia'], $data['hinh'], $data['gia_ban'], $data['chu_de_id'], $data['ma_san_pham']));
     }
@@ -38,19 +38,19 @@ class AdminModel extends Database
     }
     public function ThemLoaiSanPham($data)
     {
-        $chuoiSQL='INSERT INTO loai_san_pham(`ten_loai`, `ten_loai_san_pham_url`, `ma_loai_cha`) VALUES (?,?,0)';
+        $chuoiSQL = 'INSERT INTO loai_san_pham(`ten_loai`, `ten_loai_san_pham_url`, `ma_loai_cha`) VALUES (?,?,0)';
         $this->setQuery($chuoiSQL);
         return $this->execute(array($data['ten_loai'], $data['ten_loai_san_pham_url']));
     }
     public function ThemLoaiCon($data)
     {
-        $chuoiSQL='INSERT INTO loai_san_pham(`ten_loai`, `ten_loai_san_pham_url`, `ma_loai_cha`) VALUES (?,?,?)';
+        $chuoiSQL = 'INSERT INTO loai_san_pham(`ten_loai`, `ten_loai_san_pham_url`, `ma_loai_cha`) VALUES (?,?,?)';
         $this->setQuery($chuoiSQL);
         return $this->execute(array($data['ten_loai'], $data['ten_loai_san_pham_url'], $data['ma_loai_cha']));
     }
     public function CapNhatLoaiCha($data)
     {
-        $chuoiSQL="UPDATE loai_san_pham SET ten_loai=?, ten_loai_san_pham_url=?, ma_loai_cha=0 WHERE ma_loai=?";
+        $chuoiSQL = "UPDATE loai_san_pham SET ten_loai=?, ten_loai_san_pham_url=?, ma_loai_cha=0 WHERE ma_loai=?";
                 $this->setQuery($chuoiSQL);
         return $this->execute(array($data['ten_loai'], $data['ten_loai_san_pham_url'], $data['ma_loai']));
     }
@@ -69,7 +69,7 @@ class AdminModel extends Database
     // }
     public function CapNhatChuDe($data)
     {
-        $chuoiSQL="UPDATE chu_de SET ten_chu_de=?, ten_chu_de_url=?, mo_ta=?, hinh=? WHERE ma_chu_de=?";
+        $chuoiSQL = "UPDATE chu_de SET ten_chu_de=?, ten_chu_de_url=?, mo_ta=?, hinh=? WHERE ma_chu_de=?";
         $this->setQuery($chuoiSQL);
         return $this->execute(array($data['ten_chu_de'], $data['ten_chu_de_url'], $data['mo_ta'], $data['hinh'], $data['ma_chu_de']));
     }
